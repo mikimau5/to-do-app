3d1840b164f6a4bd9911af7f26c828992f5dfd13
Miki <alerpace@o2.pl>
2019-06-11 12:50:23 +0200

    Updates

---
+++ precache-manifest.4c94939233bc8bdb26158f6c599f9984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2213e2f726e615d3275a504a6ac7ec",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "b1fcb9ccdb5d817c5796",
+    "url": "/to-do-app/static/css/main.d22b360f.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/to-do-app/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "b1fcb9ccdb5d817c5796",
+    "url": "/to-do-app/static/js/main.a230dfeb.chunk.js"
+  },
+  {
+    "revision": "6cf4045563d5e41dd4b3",
+    "url": "/to-do-app/static/js/runtime~main.01361c46.js"
+  }
+]);
